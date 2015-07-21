958310d9d1a30fa0ee6096d47f73aa5978b74185
ganeshiverse <ganeshrajarapu.vl@gmail.com>
2015-07-21 17:00:16 +0530

    Removed support jars.

--- TCGradle/src/main/java/com/vl/tcgradletest/MainActivity.java
+++ TCGradle/src/main/java/com/vl/tcgradletest/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -28,9 +28,7 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
 }
