ce0746a382b0200188bb49be2510134e8c8a3ff0
ganeshiverse <ganeshrajarapu.vl@gmail.com>
2015-08-27 20:11:58 +0530

    Trigger build

--- TCGradle/src/main/java/com/vl/tcgradletest/MainActivity.java
+++ TCGradle/src/main/java/com/vl/tcgradletest/MainActivity.java
@@ -13,6 +13,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+	
+	 @Override
+    protected void onStart() {
+        super.onStart();
+      }
 
 
     @Override
