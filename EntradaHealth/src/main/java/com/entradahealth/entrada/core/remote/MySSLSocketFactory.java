c6c6c6171c67f103f8fd8c110b3b52d508fe5495
ganeshvl <ganeshrajarapu.vl@gmail.com>
2015-09-02 17:24:30 +0530

    Update MySSLSocketFactory.java

--- EntradaHealth/src/main/java/com/entradahealth/entrada/core/remote/MySSLSocketFactory.java
+++ EntradaHealth/src/main/java/com/entradahealth/entrada/core/remote/MySSLSocketFactory.java
@@ -39,7 +39,7 @@ public class MySSLSocketFactory extends SSLSocketFactory {
     }
 
 	public MySSLSocketFactory(SSLContext context) throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
-        super(context);
+        super(null);
         sslContext = context;
     }
 
